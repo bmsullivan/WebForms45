451fbe82f8f53f07724ff527153ee2bf169086e6
Brian Sullivan <bmsullivan@gmail.com>
2013-03-05 07:33:43 -0600

    Data annotations

--- WebFormsBank/WebFormsBank/Models/Customer.cs
+++ WebFormsBank/WebFormsBank/Models/Customer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace WebFormsBank.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Customer
     {
         public Customer()
@@ -11,12 +13,19 @@ namespace WebFormsBank.Models
         }
 
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string HomeAddress_AddressLine1 { get; set; }
+        [Required]
         public string HomeAddress_AddressLine2 { get; set; }
+        [Required]
         public string HomeAddress_City { get; set; }
+        [Required]
         public string HomeAddress_State { get; set; }
+        [Required]
         public string HomeAddress_Zip { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
         public Nullable<int> HomeBranch_Id { get; set; }
         public virtual ICollection<Account> Accounts { get; set; }
